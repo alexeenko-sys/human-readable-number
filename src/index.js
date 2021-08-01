d0d83b068c28ffb2ff9b402719f15c3c73b3a2ae
unknown <i.html06@gmail.com>
2021-08-01 10:26:43 +0600

    feat: add decision of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function toReadable (number) {
-  
+	let numberTo20 = 
+    ["zero", "one", "two", "three", "four", "five",
+	"six", "seven", "eight", "nine", "ten",
+	"eleven", "twelve", "thirteen", "fourteen", "fifteen",
+	"sixteen", "seventeen", "eighteen", "nineteen", "twenty"];
+	let numberTo90 = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+	if (number == 0){
+		return numberTo20[0];
+	}
+
+	let result = "";
+
+	if (Math.floor(number/100)){
+		result += numberTo20[Math.floor(number/100)] + " hundred";
+		number -= Math.floor(number/100)*100;
+	}
+	if (Math.floor(number/10)>1 && number != 20){
+
+		if(result.length != 0){
+			result += " ";
+		}
+
+		result += numberTo90[Math.floor(number/10)-2];
+		number -= Math.floor(number/10)*10;
+	}
+
+	if (number == 0){
+			return result;
+	}
+
+	if (result.length != 0){
+		result += " ";
+	}
+
+	return result += numberTo20[number];
 }
